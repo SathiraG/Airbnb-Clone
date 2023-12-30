0a8906306640b77a5d69e49eb465b318d5f3919d
SathiraSS <sathirassg12@gmail.com>
2023-12-30 23:05:27 +0530

    Removed Unwanted Files
    Removed Unwanted Css and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-
+export default function(){
+  return (
+    <div>
+    <h1>Test</h1>
+    <button>test</button>
+    </div>
+  )
+}
